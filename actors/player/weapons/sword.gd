e8e7a8d453b964b5c663ad2b66d05e9d1da10ab1
Dario <dario.greto@gmail.com>
2020-04-13 02:43:10 +0200

    improved sword

--- actors/player/weapons/sword.gd
+++ actors/player/weapons/sword.gd
@@ -3,11 +3,19 @@ extends Area2D
 
 export (float, 0.1, 2,  0.1) var cooldown
 
-var can_attack = true
+var can_attack := true
 
+export var scalings := {
+	"int" : 0,
+	"str" : 0,
+	"dex" : 0
+}
+
+var damage : int
 onready var player = get_parent()
 
 func _ready():
+	connect("body_entered", self, "_on_body_entered")
 	$cd.wait_time = cooldown
 	$cd.connect("timeout", self, "_on_cd_timeout")
 	$uptime.connect("timeout", self, "_on_uptime_timeout")
@@ -24,9 +32,19 @@ func _attack() -> void:
 	$Sprite.visible = true
 	can_attack = false
 	monitoring = true
+	
+	damage = calculate_damage()
+	
+
+func calculate_damage() -> int:
+	var d := 0
+	for s in scalings:
+		d += player.stats.get(s) * scalings[s]
+	return d
 
 func _on_body_entered(body) -> void:
-	pass
+	print(body.name)
+	
 
 func _on_cd_timeout() -> void:
 	can_attack = true
